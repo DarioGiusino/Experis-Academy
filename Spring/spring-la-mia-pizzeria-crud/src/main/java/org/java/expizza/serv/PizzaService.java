19f9f13a314e93a3eb71afc27c7ccb2e6c97d2ba
DarioGiusino <114605234+DarioGiusino@users.noreply.github.com>
2023-11-15 16:12:48 +0100

    Add files via upload

---
+++ Spring/spring-la-mia-pizzeria-crud/src/main/java/org/java/expizza/serv/PizzaService.java
@@ -0,0 +1,41 @@
+package org.java.expizza.serv;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.java.expizza.pojo.Pizza;
+import org.java.expizza.repo.PizzaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PizzaService {
+	
+	@Autowired
+	private PizzaRepo pizzaRepo;
+	
+	public List<Pizza> findAll() {
+		
+		return pizzaRepo.findAll();
+	}
+	
+	public Pizza save(Pizza pizza) {
+		
+		return pizzaRepo.save(pizza);
+	}
+	
+	public Optional<Pizza> findById(int id) {
+		
+		return pizzaRepo.findById(id);
+	}
+	
+	public List<Pizza> findByName(String name) {
+		
+		return pizzaRepo.findByNameContaining(name);
+	}
+	
+	public void delete(Pizza pizza) {
+		
+		pizzaRepo.delete(pizza);
+	}
+}
